e17b92df5f35069f39b4120b481a9f660b51ee23
codjoelmayer <tmjoelm4@gmail.com>
2024-02-09 16:15:46 +0200

    Express

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Welcome to Express JS");
+import express from 'express' 
+
+// Create an express app
+const app = express()
+const port = +process.env.PORT || 4000
+// Routers
+app.get('/', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: 'You\'re home'
+    })
+})
+
+app.get('/about', (req, res)=>{
+    res.json({
+        status: res.statusCode,
+        msg: 'About Page'
+    })
+})
+
+app.all('*', (req, res)=>{
+    res.json({
+        status: 404,
+        msg: '404 Page'
+    })
+})
+app.listen(port)
