36dac651bacb98743ce7140cc214b8d7aa3eb9b6
codjoelmayer <tmjoelm4@gmail.com>
2024-02-12 14:45:53 +0200

    Updates

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import express from 'express' 
-
+import path from 'path'
 // Create an express app
 const app = express()
+const router = express.Router()
+app.use(
+    router, 
+    express.static('./static')
+)
 const port = +process.env.PORT || 4000
-// Routers
-app.get('/', (req, res)=>{
-    res.json({
-        status: res.statusCode,
-        msg: 'You\'re home'
-    })
+// Routers: express
+router.get('^/$|/express', display, (req, res)=>{
+    res.status(200).sendFile(path.resolve('./static/html/index.html' ))
 })
-
-app.get('/about', (req, res)=>{
+router.get('/about', (req, res)=>{
     res.json({
         status: res.statusCode,
         msg: 'About Page'
     })
 })
+// Middleware
+function display(req, res, next){
+    console.log("Hello there");
+    next()
+}
 
-app.all('*', (req, res)=>{
-    res.json({
-        status: 404,
-        msg: '404 Page'
-    })
-})
 app.listen(port)
